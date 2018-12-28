ad61d1faeff28d76d2b131330428eb37e777252f
yurasid <yura.sid@gmail.com>
2018-12-28 11:59:19 +0200

    add firebase creds

---
+++ config.dev.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAUnjoOFn92iWVcobGjNXAm5_iQpa_eKxw",
+  authDomain: "r2order.firebaseapp.com",
+  databaseURL: "https://r2order.firebaseio.com",
+  projectId: "r2order",
+  storageBucket: "r2order.appspot.com",
+  messagingSenderId: "550434971715"
+};
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,15 +1,7 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
-
-const config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  projectId: "YOUR PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET"
-};
+import config from "../../config.dev.js";
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
